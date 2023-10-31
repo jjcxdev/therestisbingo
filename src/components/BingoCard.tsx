b5bf1bae5fa3402c6faa6fdb0284233516a2296d
jjcx <112969641+jjcxdev@users.noreply.github.com>
2023-10-31 15:49:44 -0400

    styling, free space, focus fixed

--- src/components/BingoCard.tsx
+++ src/components/BingoCard.tsx
@@ -37,6 +37,13 @@ const BingoCard: React.FC<BingoCardProps> = ({ setShowModal }) => {
   }, []);
 
   // Handle cell click to reveal image
+  const handleKeyDown = (e: React.KeyboardEvent, i: number, j: number) => {
+    if (e.key === "Enter" || e.key === " ") {
+      handleCellClick(i, j);
+      e.preventDefault();
+    }
+  };
+
   const handleCellClick = (i: number, j: number) => {
     const key = `${i}-${j}`;
     const randomIndex = Math.floor(Math.random() * dabberImages.length);
@@ -77,20 +84,23 @@ const BingoCard: React.FC<BingoCardProps> = ({ setShowModal }) => {
           {row.map((cell, j) => (
             <div
               role="gridcell"
+              tabIndex={0}
               aria-label={`Click to reveal cell ${i + 1}-${
                 j + 1
               }, contains ${cell}`}
               key={j}
               onClick={() => handleCellClick(i, j)}
-              className="text-5xs relative grid aspect-square h-full items-center justify-center gap-1 rounded-lg bg-neutral-900/80 text-center text-neutral-100 transition duration-300 lg:text-lg"
+              onKeyDown={(e) => handleKeyDown(e, i, j)}
+              className={`text-5xs relative grid aspect-square h-full items-center justify-center gap-1 rounded-lg bg-neutral-900/80 text-center text-neutral-100 transition duration-300 hover:bg-neutral-100 hover:text-neutral-900/80 focus:outline-none focus:ring-2 focus:ring-blue-500 lg:text-lg 
+              ${cell === "FREE SPACE" ? "bg-blue-500 font-tenar text-lg" : ""}`}
             >
               {revealedCells[`${i}-${j}`] && (
                 <Image
                   src={`/images/dabbers/${revealedCells[`${i}-${j}`]}`}
-                  width={10}
-                  height={10}
                   alt="A revealed bingo dabber image"
-                  className="absolute inset-x-0 inset-y-0 z-10 mx-auto my-auto flex h-3/4 w-3/4 items-center justify-center"
+                  width={100}
+                  height={100}
+                  className="absolute inset-x-0 inset-y-0 z-10 mx-auto my-auto flex h-fit w-fit items-center justify-center"
                 />
               )}
               <span className="3xl:max-w-[150px] 4xl:text-4xl 4xl:max-w-[300px] max-w[50px] z-0 whitespace-normal break-words p-[2px] text-xs lg:max-w-[100px] lg:p-2 xl:max-w-[110px] xl:text-sm 2xl:max-w-[125px]">
