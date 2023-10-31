912637e744729b6c2fbdbb25681ff603f1fb715a
jjcx <112969641+jjcxdev@users.noreply.github.com>
2023-10-31 16:49:00 -0400

    opengraph added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,40 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     <html lang="en">
       <Head>
         <title>The Rest is BINGO</title>
+        <meta
+          name="description"
+          content="Join in for a cheeky bingo game whilst tuning into the latest episodes of 'The Rest is Football'. A brilliant mate for keen listeners!"
+        />
+
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content="http://therestisbingo.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="The Rest is BINGO" />
+        <meta
+          property="og:description"
+          content="Join in for a cheeky bingo game whilst tuning into the latest episodes of 'The Rest is Football'. A brilliant mate for keen listeners!"
+        />
+        <meta
+          property="og:image"
+          content="http://therestisbingo.vercel.app/images/bingoScreenshot.png"
+        />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="therestisbingo.vercel.app" />
+        <meta
+          property="twitter:url"
+          content="http://therestisbingo.vercel.app/"
+        />
+        <meta name="twitter:title" content="The Rest is BINGO" />
+        <meta
+          name="twitter:description"
+          content="Join in for a cheeky bingo game whilst tuning into the latest episodes of 'The Rest is Football'. A brilliant mate for keen listeners!"
+        />
+        <meta
+          name="twitter:image"
+          content="http://therestisbingo.vercel.app/images/bingoScreenshot.png"
+        />
       </Head>
       <body>
         <Component {...pageProps} />
